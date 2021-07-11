3e0496bb99d18e16615722da60a3c53c189fba3e
SergeyArtemyev <sorg.true@gmail.com>
2021-07-11 17:07:24 +0300

    mock transport.request

---
+++ __tests__/genesis.test.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const fsp = require('fs').promises
+const { Genesis } = require('../index')
+jest.mock('axios')
+
+test('should return something', async () => {
+  const answer = { RESULT: '1', ERROR: '3' }
+  const response = { RESULT: '1', ERROR: '3' }
+  // axios.post.mockResolvedValue(response)
+
+  const transport = new Genesis({
+    defaultFields: {
+      AP: 1111
+    },
+    provider: {
+      url: 'https://predproc.tothemars.io/pay'
+    },
+    crypto: {
+      key: await fsp.readFile('../simple-cyberplat/secret/key.pem', 'utf-8'),
+      passphrase: ''
+    }
+  })
+
+  axios.post.mockResolvedValue(response)
+  const result = await transport.request({
+    REQ_TYPE: 12
+  })
+
+  expect(result).toEqual(answer)
+})
---
+++ genesis.js
@@ -0,0 +1,32 @@
+const fsp = require('fs').promises
+const { Genesis } = require('./index')
+
+const wrapAsync = f => f().catch(console.log)
+
+const getTransport = async () => {
+  try {
+    const transport = new Genesis({
+      defaultFields: {
+        AP: 1111
+      },
+      provider: {
+        url: 'https://predproc.tothemars.io/pay'
+      },
+      crypto: {
+        key: await fsp.readFile('./secret/key.pem', 'utf-8'),
+        passphrase: ''
+      }
+    })
+
+    const result = await transport.request({
+      REQ_TYPE: 12
+    })
+
+    return result.data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+wrapAsync(getTransport)
+module.exports = getTransport
